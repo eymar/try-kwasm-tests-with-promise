f42f3fbbc85ee6fb9d6b2aa6d30b9d0cc69f338a
Oleksandr Karpovich <a.n.karpovich@gmail.com>
2024-09-02 11:42:20 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform) 
+}
+
+group = "me.user"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    wasmJs {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        add(project.rootDir.path)
+                    }
+                }
+            }
+        }
+    }
+
+    sourceSets {
+      val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+        val wasmJsMain by getting
+        val wasmJsTest by getting
+    }
+}
